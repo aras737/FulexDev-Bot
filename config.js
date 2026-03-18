2846e8debf49265c2106e057b2a7f6b80e8f3f28
aras737 <erdemiraraseymen936@gmail.com>
2026-03-18 12:23:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     token: process.env.TOKEN, // Buraya dokunma, Railway'deki Variables kısmından çekecek
     prefix: "tfa!",
     botdavet: "BOT_DAVET_BURAYA",
-    desteksunucusu: "BOT_DESTEK_SUNUCUSU_BURAYA",
+    desteksunucusu: "https://discord.gg/nuxbuPBQh",
     website: "BOT_WEBSITE_BURAYA",
     topgg: "BOT_TOPGG_KEY_BURAYA",
     politika: "BOT_TOS_BURAYA",
