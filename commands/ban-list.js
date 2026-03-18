969be0d86448edab3bfabe1b9596d4a0b713f740
aras737 <erdemiraraseymen936@gmail.com>
2026-03-18 22:32:39 +0300

    Update ban-list.js

--- commands/ban-list.js
+++ commands/ban-list.js
@@ -1,33 +1,46 @@
-const {EmbedBuilder} = require("discord.js");
-const moment = require("moment");
-  require("moment-duration-format");
-  const os = require("os");
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
 exports.run = async (client, message, args) => {
-    var userlist = message.guild.bans.fetch()
-    userlist.then(collection => {
-    if(collection.first() == null){
-      
-    const embed = new MessageEmbed()
-    .setDescription("Sunucunuzda Banlanan Kimse Yok!")      
-    .setColor("RED")
-    .setTitle(":x: Hata!")
-    message.channel.send({embeds: [embed]})
-      
-    } else {
-    const data = collection.map(mr => "`"+mr.user.username+"`").slice(0, 60).join(", ")
-    const embed2 = new EmbedBuilder()
-    .setTitle("ErensiBOT - Ban List")
-    .setColor("#ff0000")
-    .setDescription(data)
-    
-    message.channel.send({embeds: [embed2]})
+    // Yetki kontrolü
+    if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+        return message.reply("❌ | Bu komutu kullanmak için **Üyeleri Yasakla** yetkisine sahip olmalısın!");
     }
-})
+
+    // Yasaklı listesini çekiyoruz
+    message.guild.bans.fetch().then(bans => {
+        if (bans.size === 0) {
+            const errorEmbed = new EmbedBuilder()
+                .setTitle("❌ Hata!")
+                .setColor("#ff0000")
+                .setDescription("Sunucunuzda yasaklanmış kullanıcı bulunmuyor.");
+            return message.channel.send({ embeds: [errorEmbed] });
+        }
+
+        // Görseldeki gibi numaralı ve detaylı liste oluşturma
+        // İlk 15 kişiyi gösteriyoruz (Mesaj karakter sınırına takılmamak için)
+        const data = bans.map((ban, index) => {
+            return `**${index + 1}. ${ban.user.tag}**\n**ID:** ${ban.user.id}\n**Sebep:** ${ban.reason || "Sebep belirtilmedi"}`;
+        }).slice(0, 15).join("\n\n");
+
+        const listEmbed = new EmbedBuilder()
+            .setColor("#e74c3c") // Görseldeki kırmızı tonu
+            .setTitle("Yasaklı Kullanıcı Listesi")
+            .setDescription(`Toplam yasaklı kullanıcı: ${bans.size}\n\n${data}`)
+            .setFooter({ text: `${client.user.username} Ban Sistemi`, iconURL: client.user.displayAvatarURL() })
+            .setTimestamp();
+
+        message.channel.send({ embeds: [listEmbed] });
+    }).catch(err => {
+        console.error(err);
+        message.reply("❌ | Yasaklı listesi alınırken bir hata oluştu.");
+    });
 };
+
 exports.conf = {
-  aliases: []
+    aliases: ["banlist", "yasaklılar"],
+    permLevel: 0
 };
 
 exports.help = {
-  name: "ban-list"
+    name: "ban-list"
 };
